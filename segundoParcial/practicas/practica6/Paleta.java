bf64771ef1ce572f908d8ba74ca191856b41061f
David7033 <7033@itescam.edu.mx>
2021-07-14 18:39:12 -0500

    Pong 1 jugador

---
+++ segundoParcial/practicas/practica6/Paleta.java
@@ -0,0 +1,13 @@
+package segundoParcial.practicas.practica6;
+
+public class Paleta {
+    public int y;
+    public int alto;
+    
+   
+    public Paleta(int y, int alto) {
+        this.y = y;
+        this.alto = alto;
+        
+    }
+}
