fe4f813220e184de7ba0bc75c1e863e2dd2628a1
David7033 <7033@itescam.edu.mx>
2021-07-14 00:01:04 -0500

    Hilos

---
+++ segundoParcial/practicas/practica4/HiloServidor.java
@@ -0,0 +1,53 @@
+package segundoParcial.practicas.practica4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.io.InputStreamReader;
+public class HiloServidor extends Thread
+{
+    BufferedReader entrada;
+    PrintWriter salida;
+    Socket socket = null;
+    
+    public HiloServidor (Socket s)
+    {
+        socket=s;
+        try
+        {
+            salida = new PrintWriter(socket.getOutputStream(),true);
+            entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    public void run()
+    {
+        String cadena = "";
+        while(!cadena.trim().equals("")){
+            System.out.println("Hablando con: "+socket.toString());
+            try
+            {
+                cadena=entrada.readLine();
+                if(cadena!= null)
+                {
+                    salida.println(cadena.trim().toUpperCase());
+                }
+            }catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+        System.out.println("Desconectandose: "+socket.toString());
+        try
+        {
+            salida.close();
+            entrada.close();
+            socket.close();
+        }catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
